04115578075bac6b5e1ee0a1f685f5eda3148120
Chris Pulman <chris.pulman@yahoo.com>
2023-05-22 22:53:01 +0100

    Make default theme as bootstrap recommendation dark

--- input/assets/js/color-modes.js
+++ input/assets/js/color-modes.js
@@ -14,12 +14,12 @@
       return storedTheme
     }
 
-      return window.matchMedia('(prefers-color-scheme: light)').matches ? 'dark' : 'light'
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
   }
 
   const setTheme = function (theme) {
-      if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: light)').matches) {
-          document.documentElement.setAttribute('data-bs-theme', 'light')
+    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.setAttribute('data-bs-theme', 'dark')
     } else {
       document.documentElement.setAttribute('data-bs-theme', theme)
     }
